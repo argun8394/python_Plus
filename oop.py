ee815a70767a9a805c82fdda6d45a0e15b31a631
argun8394 <argundoganali@gmail.com>
2021-10-06 22:48:13 +0300

    Class Attributes ve instance attributes

--- oop.py
+++ oop.py
@@ -28,3 +28,35 @@ test =[123, "Barry", [1,2,3], (1,2,3), {1,2,3}, True, lambda x: x,{
 
 # print(person1.job)
 # print(person2.job)
+
+# #  Class Attributes ve instance attributes
+
+
+# person1.name = "Henry"
+# Person.name = "Rafe"
+
+# print(person1.name)
+# print(person2.name)
+
+# Self Keyword
+
+class Person:
+    name = "Barry"
+    age = 44
+
+    def test(self):
+        print("test")
+
+    def get_details(self):
+        print("name: ", self.name, "age", self.age, "location :" , self.location )
+
+    def set_details(self, name, age, location):
+        self.name = name
+        self.age = age
+        self.location = location
+
+person1 = Person()
+person1.test()
+Person.test(person1)
+person1.set_details("Henry", 37, "Ankara")
+person1.get_details()
